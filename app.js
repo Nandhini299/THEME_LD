b2b8a0de7debcf6751cf35515973f216630cae86
Nandhini299 <nandhinni14082005@gmail.com>
2025-03-19 23:46:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// var dark_btn = document.getElementById("dark");
+// var light_btn = document.getElementById("light");
+
+// function
+var body = document.querySelector("body");
+function dark_theme()
+{
+    body.className ="";
+    body.classList.add("dark");
+}
+
+function light_theme()
+{
+    body.className="";
+    body.classList.add("light");
+}
